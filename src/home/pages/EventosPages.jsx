59a05923dae8204603e55f590bff7cd25f6baf00
Camilo Ortiz <camiloa.ortizm@gmail.com>
2023-05-30 16:48:52 -0400

    ajustar tamaño card de eventos en home

--- src/home/pages/EventosPages.jsx
+++ src/home/pages/EventosPages.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'
 import { CBreadcrumb, CBreadcrumbItem, CCard, CCardBody, CCardImage, CCardText, CCardTitle, CCol, CContainer, CHeaderDivider, CRow } from '@coreui/react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Pagination, Autoplay } from "swiper";
@@ -38,36 +37,39 @@ export const EventosPages = () => {
                 <CRow className="g-5">
                     {eventosHome.map((evento) => (
                         <CCol xl={6} key={evento.id}>
-                            <CCard className="mb-4">
+                            <CCard className="card-evento">
                                 <CCardBody>
-                                    <Swiper
-                                        modules={[Navigation, Pagination, Autoplay]}
-                                        breakpoints={{
-                                            1200: {
-                                                slidesPerView: 1,
-                                                spaceBetween: 30,
-                                            }
-                                        }}
-                                        spaceBetween={50}
-                                        navigation={true} 
-                                        slidesPerView={1}
-                                        autoplay={{
-                                            delay: 2500,
-                                            disableOnInteraction: false,
-                                        }}
-                                        pagination={{
-                                            clickable: true,
-                                        }}    
-                                    >
-                                        {evento.archivos.map((archivo) => (
-                                            <SwiperSlide key={archivo.id}>
-                                                <CCardImage className='imagen-portada' src={`http://134.122.124.97/storage/${archivo.url}`} alt={`Imagen ${archivo.id}`} />
-                                            </SwiperSlide>
-                                        ))}
-                                    </Swiper>
-                                    <CCardTitle className='text-center'>{evento.titulo_evento}</CCardTitle>
-                                    <CCardText dangerouslySetInnerHTML={{ __html: evento.descripcion_evento }}>
-                                    </CCardText>
+                                    <div className='card_imagen-evento'>
+                                        <Swiper
+                                            modules={[Navigation, Pagination, Autoplay]}
+                                            breakpoints={{
+                                                1200: {
+                                                    slidesPerView: 1,
+                                                    spaceBetween: 30,
+                                                }
+                                            }}
+                                            spaceBetween={50}
+                                            navigation={true} 
+                                            slidesPerView={1}
+                                            autoplay={{
+                                                delay: 2500,
+                                                disableOnInteraction: false,
+                                            }}
+                                            pagination={{
+                                                clickable: true,
+                                            }}    
+                                        >
+                                            {evento.archivos.map((archivo) => (
+                                                <SwiperSlide key={archivo.id}>
+                                                    <CCardImage className='imagen-evento' src={`http://134.122.124.97/storage/${archivo.url}`} alt={`Imagen ${archivo.id}`} />
+                                                </SwiperSlide>
+                                            ))}
+                                        </Swiper>
+                                    </div>
+                                    <div className='card_descripcion_evento'>
+                                        <CCardTitle className='text-center'>{evento.titulo_evento}</CCardTitle>
+                                        <CCardText dangerouslySetInnerHTML={{ __html: evento.descripcion_evento }}></CCardText>
+                                    </div>
                                 </CCardBody>
                             </CCard>
                         </CCol>
@@ -104,7 +106,7 @@ export const EventosPages = () => {
                                     >
                                         {evento.archivos.map((archivo) => (
                                             <SwiperSlide key={archivo.id}>
-                                                <CCardImage className='imagen-portada' src={`http://134.122.124.97/storage/${archivo.url}`} alt={`Imagen ${archivo.id}`} />
+                                                <CCardImage className='imagen-evento' src={`http://134.122.124.97/storage/${archivo.url}`} alt={`Imagen ${archivo.id}`} />
                                             </SwiperSlide>
                                         ))}
                                     </Swiper>
